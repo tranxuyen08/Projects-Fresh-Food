3328c17b82629e0ffeb414653104efa0a9772dfb
tranxuyen08 <82137908+tranxuyen08@users.noreply.github.com>
2023-05-09 14:06:09 +0700

    fix db

--- javascript/db.js
+++ javascript/db.js
@@ -1,1 +1,163 @@
+const products = [
+  {
+    id: 1,
+    image: "image/1000_F_563984104_iGjGlcHA9p9FIRh2edt9VUsNkMBgVgAt.jpg",
+    type: "vegetabel",
+    name: "Name Product",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporincididunt ut laboreet dolore magna aliqua. Ut enim ad minim veniam, quis nstrud exercitation ullamco laboris...",
+    qty: 0,
+    sale: "",
+    price: 25000
+  },
+  {
+    id: 2,
+    image: "image/1000_F_563984104_iGjGlcHA9p9FIRh2edt9VUsNkMBgVgAt.jpg",
+    type: "vegetabel",
+    name: "Name Product",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporincididunt ut laboreet dolore magna aliqua. Ut enim ad minim veniam, quis nstrud exercitation ullamco laboris...",
+    qty: 0,
+    sale: 40,
+    price: 25000
+  },
+  {
+    id: 3,
+    image: "image/1000_F_563984104_iGjGlcHA9p9FIRh2edt9VUsNkMBgVgAt.jpg",
+    type: "vegetabel",
+    name: "Name Product",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporincididunt ut laboreet dolore magna aliqua. Ut enim ad minim veniam, quis nstrud exercitation ullamco laboris...",
+    qty: 0,
+    sale: "",
+    price: 25000
+  },
+  {
+    id: 4,
+    image: "image/1000_F_563984104_iGjGlcHA9p9FIRh2edt9VUsNkMBgVgAt.jpg",
+    type: "vegetabel",
+    name: "Name Product",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporincididunt ut laboreet dolore magna aliqua. Ut enim ad minim veniam, quis nstrud exercitation ullamco laboris...",
+    qty: 0,
+    sale: "",
+    price: 25000
+  },
+  {
+    id: 5,
+    image: "image/1000_F_563984104_iGjGlcHA9p9FIRh2edt9VUsNkMBgVgAt.jpg",
+    type: "vegetabel",
+    name: "Name Product",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporincididunt ut laboreet dolore magna aliqua. Ut enim ad minim veniam, quis nstrud exercitation ullamco laboris...",
+    qty: 0,
+    sale: 25,
+    price: 25000
+  },
+  {
+    id: 6,
+    image: "image/1000_F_563984104_iGjGlcHA9p9FIRh2edt9VUsNkMBgVgAt.jpg",
+    type: "vegetabel",
+    name: "Name Product",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporincididunt ut laboreet dolore magna aliqua. Ut enim ad minim veniam, quis nstrud exercitation ullamco laboris...",
+    qty: 0,
+    sale: 15,
+    price: 25000
+  },
+  {
+    id: 7,
+    image: "image/1000_F_563984104_iGjGlcHA9p9FIRh2edt9VUsNkMBgVgAt.jpg",
+    type: "vegetabel",
+    name: "Name Product",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporincididunt ut laboreet dolore magna aliqua. Ut enim ad minim veniam, quis nstrud exercitation ullamco laboris...",
+    qty: 0,
+    sale: "",
+    price: 25000
+  },
+  {
+    id: 8,
+    image: "image/1000_F_563984104_iGjGlcHA9p9FIRh2edt9VUsNkMBgVgAt.jpg",
+    type: "vegetabel",
+    name: "Name Product",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporincididunt ut laboreet dolore magna aliqua. Ut enim ad minim veniam, quis nstrud exercitation ullamco laboris...",
+    qty: 0,
+    sale: "",
+    price: 25000
+  },
+  {
+    id: 9,
+    image: "image/1000_F_563984104_iGjGlcHA9p9FIRh2edt9VUsNkMBgVgAt.jpg",
+    type: "vegetabel",
+    name: "Name Product",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporincididunt ut laboreet dolore magna aliqua. Ut enim ad minim veniam, quis nstrud exercitation ullamco laboris...",
+    qty: 0,
+    sale: "",
+    price: 25000
+  },
+  {
+    id: 10,
+    image: "image/1000_F_563984104_iGjGlcHA9p9FIRh2edt9VUsNkMBgVgAt.jpg",
+    type: "vegetabel",
+    name: "Name Product",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporincididunt ut laboreet dolore magna aliqua. Ut enim ad minim veniam, quis nstrud exercitation ullamco laboris...",
+    qty: 0,
+    sale: "",
+    price: 25000
+  },
+  {
+    id: 11,
+    image: "image/1000_F_563984104_iGjGlcHA9p9FIRh2edt9VUsNkMBgVgAt.jpg",
+    type: "vegetabel",
+    name: "Name Product",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporincididunt ut laboreet dolore magna aliqua. Ut enim ad minim veniam, quis nstrud exercitation ullamco laboris...",
+    qty: 0,
+    sale: 35,
+    price: 25000
+  },
+  {
+    id: 12,
+    image: "image/1000_F_563984104_iGjGlcHA9p9FIRh2edt9VUsNkMBgVgAt.jpg",
+    type: "vegetabel",
+    name: "Name Product",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporincididunt ut laboreet dolore magna aliqua. Ut enim ad minim veniam, quis nstrud exercitation ullamco laboris...",
+    qty: 0,
+    sale: 35,
+    price: 25000
+  },
+  {
+    id: 13,
+    image: "image/1000_F_563984104_iGjGlcHA9p9FIRh2edt9VUsNkMBgVgAt.jpg",
+    type: "vegetabel",
+    name: "Name Product",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporincididunt ut laboreet dolore magna aliqua. Ut enim ad minim veniam, quis nstrud exercitation ullamco laboris...",
+    qty: 0,
+    sale: 35,
+    price: 25000
+  },
+  {
+    id: 14,
+    image: "image/1000_F_563984104_iGjGlcHA9p9FIRh2edt9VUsNkMBgVgAt.jpg",
+    type: "vegetabel",
+    name: "Name Product",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporincididunt ut laboreet dolore magna aliqua. Ut enim ad minim veniam, quis nstrud exercitation ullamco laboris...",
+    qty: 0,
+    sale: "",
+    price: 25000
+  },
+  {
+    id: 15,
+    image: "image/1000_F_563984104_iGjGlcHA9p9FIRh2edt9VUsNkMBgVgAt.jpg",
+    type: "vegetabel",
+    name: "Name Product",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporincididunt ut laboreet dolore magna aliqua. Ut enim ad minim veniam, quis nstrud exercitation ullamco laboris...",
+    qty: 0,
+    sale: 35,
+    price: 25000
+  },
+  {
+    id: 16,
+    image: "image/1000_F_563984104_iGjGlcHA9p9FIRh2edt9VUsNkMBgVgAt.jpg",
+    type: "vegetabel",
+    name: "Name Product",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporincididunt ut laboreet dolore magna aliqua. Ut enim ad minim veniam, quis nstrud exercitation ullamco laboris...",
+    qty: 0,
+    sale: "",
+    price: 25000
+  },
+]
 localStorage.setItem('products', JSON.stringify(products)) ?? [];
