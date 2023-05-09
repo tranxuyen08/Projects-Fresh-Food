bd80968feda28408214441376f1cd91f8804fe73
tranxuyen08 <82137908+tranxuyen08@users.noreply.github.com>
2023-05-09 14:03:51 +0700

    set db

--- javascript/db.js
+++ javascript/db.js
@@ -1,3 +1,1 @@
-const productsDB = localStorage.setItem('products', JSON.stringify(products)) ?? [];
-const accounts = localStorage.setItem('accounts', JSON.stringify(accounts)) ?? [];
-const userAccounts = localStorage.setItem('user', JSON.stringify(user)) ?? [];
+localStorage.setItem('products', JSON.stringify(products)) ?? [];
